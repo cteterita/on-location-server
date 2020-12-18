2dc5724845a558494dd30547192eca0e1f0a8465
Claire Lesh <cteterita@gmail.com>
2020-12-17 18:08:15 -0800

    added seed and migration scripts

---
+++ seeds/seed.pins.sql
@@ -0,0 +1,6 @@
+INSERT INTO pins
+    ( title, media_type, link, lat, lon )
+VALUES
+    ( 'The Joy Luck Club', 'book', 'https://www.goodreads.com/book/show/7763.The_Joy_Luck_Club', 37.7790262, -122.4199061 ),
+    ( 'The Wild Parrots of Telegraph Hill', 'book', 'https://www.goodreads.com/book/show/221682.The_Wild_Parrots_of_Telegraph_Hill?ac=1&from_search=true&qid=ygucMo2g7U&rank=1', 37.800785, -122.4040908 ),
+    ( 'The Rock', 'movie', 'https://www.imdb.com/title/tt0117500/', 37.82672135, -122.42275899441964 );
